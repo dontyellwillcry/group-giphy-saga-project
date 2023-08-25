d4ccdd80bd269a3af4fdf9de3e05fe58447bcd5d
LazyDotExe <papaporo@proton.me>
2023-08-24 20:06:20 -0500

    updated the giphy list so they will display on the dom.

--- src/components/GiphyList/GiphyList.jsx
+++ src/components/GiphyList/GiphyList.jsx
@@ -1,40 +1,54 @@
 import React, { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 
-
 function GiphyList() {
-    console.log("in GiphyList TADA")
+  console.log("in GiphyList TADA");
 
-    const dispatch = useDispatch();
-    const giphySearchList = useSelector((store) => store.giphySearchList)
+  const dispatch = useDispatch();
+  const giphySearchList = useSelector((store) => store.giphySearchList);
 
-    //let testGiph = giphySearchList.data
-    //console.log("?",giphySearchList.data)
-    //console.log("gSL",giphySearchList.data.data[0].id)
-    // useEffect(() => {
-    //     // getGiphs()
-    // }, [])
+  //let testGiph = giphySearchList.data
 
-    const getGiphs = () => {
+//   console.log("gSL",giphySearchList.data[0].images.original.url)
+  //   useEffect(() => {
+  //       getGiphs()
+  //   }, [])
 
-        console.log('in getGiphs"')
-        dispatch({ type: 'FETCH_GIPHS' })
-        
+  // There was actaully nothing wrong with our console logs
+  // the problem was WHERE we were console logging.
+  // Any console log needed to happend below the dispatch.
 
-    }
+  const getGiphs = () => {
+    console.log('in getGiphs"');
+    dispatch({ type: "FETCH_GIPHS" });
+    // console.log("gSL",giphySearchList.data[0].images.original.url)
+  };
 
-
-    return (
-        <>
-            <div>
-                <input id="searchForm" type="text" placeholder="search"></input>
-                <button className="searchButton" onClick={getGiphs}>SEARCH</button>
-            </div>
-            <div>
-                <img src={giphySearchList.data[1].url} alt="GIF" />
-            </div>
-        </>
-    )
+  return (
+    <>
+      <div>
+        <input id="searchForm" type="text" placeholder="search"></input>
+        <button className="searchButton" onClick={getGiphs}>
+          SEARCH
+        </button>
+      </div>
+      <div>
+        {giphySearchList.data ? (
+          <ul>
+            {/* We have to map over .data because even though giphySearchList is an array, 
+            .data is a nested array */}
+            {giphySearchList.data.map((gif) => (
+              <li key={gif.id}>
+                <img src={gif.images.original.url} alt={gif.title} />
+              </li>
+            ))}
+          </ul>
+        ) : (
+          <p>No GIFs to display</p>
+        )}
+      </div>
+    </>
+  );
 }
 
 export default GiphyList;
