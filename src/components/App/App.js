3611383806b32439ff4b8815d1918dfbeb6facad
Hannah Comparetto <hannahcomparetto@gmail.com>
2023-08-25 10:25:11 -0500

    create links for giphy search and giphy fav

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,15 +1,27 @@
 import React from 'react';
 import GiphyList from '../GiphyList/GiphyList.jsx';
+import GiphyFavorites from '../GiphyFavorites/GiphyFavorites.jsx';
 import './App.css';
+import { HashRouter as Router, Route, Link } from "react-router-dom";
 
 function App(props) {
   return (
-    <div>
+    
+    <Router>
+      <div>
+      <Route path="/favorites" exact>
+      <GiphyFavorites />
+      </Route>
+      
+      <Route path="/" exact>
+      <Link to="/favorites">Go To Favorite Giphs ⭐️</Link>
       <header>
       <h1>Giphy Search!</h1>
       </header>
       <GiphyList />
-    </div>
+      </Route>
+      </div>
+    </Router>
   );
 }
 
--- src/components/GiphyFavorites/GiphyFavorites.jsx
+++ src/components/GiphyFavorites/GiphyFavorites.jsx
@@ -1,5 +1,6 @@
 import React, {useEffect} from "react";
 import { useDispatch, useSelector } from "react-redux";
+import { HashRouter as Router, Route, Link } from "react-router-dom";
 
 
 function GiphyFavorites() {
@@ -9,14 +10,12 @@ function GiphyFavorites() {
     const changeCategory = () => {
         dispatch({
           type: "CHANGE_CAT",
-          payload: {gif.id,
-                    cat.id
- }
-
+          // payload: {gif.id,cat.id}
         });
       };
 
     return (<>
+    <Link to="/">🔙 Go Back To Search Giphs</Link>
     <h1>Inside of Gfaves</h1>
             {/* <li> .map over FavList here </li> 
             <input type="dropdown"/> for category*/}
